c5422c5ae1c6bc1d89b6e762a44158cd01142c6b
Michał Szwindowski <github@nedi.me>
2026-05-23 22:08:10 +0200

    fix(frontend): clean normal event lint issues

--- src/components/NormalEvent.tsx
+++ src/components/NormalEvent.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { observer } from "mobx-react";
 
-import { NormalEventModel } from "../models/NormalEvent";
 import { EventState } from "../models/Event";
+import type { NormalEventModel } from "../models/NormalEvent";
 
-const NormalEvent: React.FC<INormalEventProps> = function ({ images, event }) {
-  return (
-    <div className={"event" + (event.state === EventState.PREPARE ? " center" : " right")}>
-      {images.hasOwnProperty(event.key) && (
-        <div className={"image"}>
-          <img src={images[event.key]} alt={""} />
-        </div>
-      )}
-      <div className={"desc"}>{event.description}</div>
-      {typeof event.time !== "undefined" && <div className={"timer"}>{event.formattedTime}</div>}
-    </div>
-  );
-};
+const objectHasOwn = (Object as unknown as {
+  hasOwn: (object: object, property: PropertyKey) => boolean;
+}).hasOwn;
+
+const NormalEvent: React.FC<INormalEventProps> = ({ images, event }) => (
+  <div className={`event${event.state === EventState.PREPARE ? " center" : " right"}`}>
+    {objectHasOwn(images, event.key) && (
+      <div className="image">
+        <img src={images[event.key]} alt="" />
+      </div>
+    )}
+    <div className="desc">{event.description}</div>
+    {typeof event.time !== "undefined" && <div className="timer">{event.formattedTime}</div>}
+  </div>
+);
 
 interface INormalEventProps {
-  images: { [key: string]: any };
+  images: Record<string, string>;
   event: NormalEventModel;
 }
 
