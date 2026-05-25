b6142fae8721dae65a48fdf131e1f14534285693
Michał Szwindowski <github@nedi.me>
2026-05-25 22:11:18 +0200

    refactor(goals): convert Goal to function component

--- src/components/Goal.tsx
+++ src/components/Goal.tsx
@@ -1,239 +1,54 @@
-import React from "react";
-import { makeObservable, observable, reaction } from "mobx";
-import type { IReactionDisposer } from "mobx";
-import { observer } from "mobx-react";
+import { useCallback, useEffect, useRef } from "react";
 import { resolveGoalImageUrl } from "../assets/resolveOverlayAssetUrl";
 
-@observer
-export default class Goal extends React.Component<IGoalProps> {
-  private static CIRCLE_MAX_SIZE = 790;
-  private static LINE_WIDTH = 35;
-  private static INNER_LINE_COLOR = "#dddddd";
+const CIRCLE_MAX_SIZE = 790;
+const LINE_WIDTH = 35;
+const INNER_LINE_COLOR = "#dddddd";
 
-  private readonly canvasRef: React.RefObject<HTMLCanvasElement> = React.createRef();
+function applyAngle(
+  point: { x: number; y: number },
+  angle: number,
+  distance: number,
+) {
+  return {
+    x: point.x + Math.cos(angle) * distance,
+    y: point.y + Math.sin(angle) * distance,
+  };
+}
 
-  private canvasContext?: CanvasRenderingContext2D;
+export default function Goal({ current, goal, type }: IGoalProps) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasContextRef = useRef<CanvasRenderingContext2D>();
+  const canvasSizeRef = useRef(CIRCLE_MAX_SIZE);
+  const centerPositionRef = useRef<{ x: number; y: number }>();
+  const backgroundLoadedRef = useRef(false);
+  const backgroundImageRef = useRef<HTMLImageElement>();
+  const resizeTimeoutRef = useRef<ReturnType<typeof setTimeout>>();
+  const savedPixelsRef = useRef<Uint8ClampedArray>();
+  const isMountedRef = useRef(false);
+  const goalPercentageRef = useRef(current / goal);
 
-  private canvasSize: number = Goal.CIRCLE_MAX_SIZE;
-  private centerPosition?: { x: number; y: number };
-
-  private backgroundLoaded = false;
-  private backgroundImage = new Image();
-
-  private resizeTimeout?: ReturnType<typeof setTimeout>;
-  private savedPixels?: Uint8ClampedArray;
-  private isComponentMounted = false;
-
-  private disposeCurrentReaction?: IReactionDisposer;
-  private disposeGoalReaction?: IReactionDisposer;
-
-  goalPercentage: number = 0;
-
-  constructor(props: IGoalProps) {
-    super(props);
-
-    this.goalPercentage = this.props.current / this.props.goal;
-
-    makeObservable(this, {
-      goalPercentage: observable,
-    });
-
-    this.disposeCurrentReaction = reaction(
-      () => this.props.current,
-      (current) => {
-        this.goalPercentage = current / this.props.goal;
-
-        if (this.goalPercentage > 1) this.goalPercentage = 1;
-        else this.draw();
-      },
-    );
-
-    this.disposeGoalReaction = reaction(
-      () => this.props.goal,
-      (goal) => {
-        this.goalPercentage = this.props.current / goal;
-
-        if (this.goalPercentage > 1) this.goalPercentage = 1;
-        else this.draw();
-      },
-    );
+  if (!backgroundImageRef.current) {
+    backgroundImageRef.current = new Image();
   }
 
-  componentDidMount() {
-    this.isComponentMounted = true;
-    this.setup();
+  const drawProgress = useCallback(() => {
+    const canvasContext = canvasContextRef.current;
+    const centerPosition = centerPositionRef.current;
 
-    window.addEventListener("resize", this.onResize);
-  }
+    if (!canvasContext || !centerPosition) return;
 
-  componentWillUnmount() {
-    this.isComponentMounted = false;
-
-    this.disposeCurrentReaction?.();
-    this.disposeCurrentReaction = undefined;
-    this.disposeGoalReaction?.();
-    this.disposeGoalReaction = undefined;
-
-    if (this.resizeTimeout) {
-      clearTimeout(this.resizeTimeout);
-      this.resizeTimeout = undefined;
-    }
-
-    this.savedPixels = undefined;
-    this.backgroundImage.onload = null;
-
-    window.removeEventListener("resize", this.onResize);
-  }
-
-  render() {
-    const { current, goal, type } = this.props;
-
-    return (
-      <div className={"goal"}>
-        {type === "subs" && <h5>Cel subów</h5>}
-        {type === "followers" && <h5>Cel followów</h5>}
-
-        <canvas ref={this.canvasRef} />
-
-        <div className={"text"}>
-          {current} / {goal}
-        </div>
-      </div>
-    );
-  }
-
-  private setup() {
-    if (!this.isComponentMounted) return;
-
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-
-    const toOperate = windowWidth < windowHeight ? windowWidth : windowHeight;
-
-    if (toOperate <= Goal.CIRCLE_MAX_SIZE) {
-      this.canvasSize = toOperate;
-    } else {
-      if (this.canvasSize !== Goal.CIRCLE_MAX_SIZE) this.canvasSize = Goal.CIRCLE_MAX_SIZE;
-    }
-
-    if (!this.canvasRef.current) return;
-
-    this.savedPixels = undefined;
-    this.canvasRef.current.width = this.canvasSize;
-    this.canvasRef.current.height = this.canvasSize;
-
-    this.centerPosition = { x: this.canvasSize / 2, y: this.canvasSize / 2 };
-
-    if (!this.canvasContext) {
-      this.canvasContext = this.canvasRef.current.getContext("2d", { willReadFrequently: true });
-    }
-
-    if (this.backgroundLoaded) this.draw();
-    else {
-      this.backgroundImage.src = resolveGoalImageUrl("kaaajk4-love");
-      this.backgroundImage.onload = () => {
-        if (!this.isComponentMounted) return;
-
-        this.backgroundLoaded = true;
-        this.draw();
-      };
-    }
-  }
-
-  private draw() {
-    if (!this.isComponentMounted || !this.canvasContext || !this.centerPosition) return;
-
-    this.canvasContext.clearRect(0, 0, this.canvasSize, this.canvasSize);
-
-    this.canvasContext.save();
-
-    this.canvasContext.beginPath();
-    this.canvasContext.arc(
-      this.centerPosition.x,
-      this.centerPosition.y,
-      this.canvasSize / 2 - Goal.LINE_WIDTH,
-      0,
-      2 * Math.PI,
-      false,
-    );
-    this.canvasContext.closePath();
-    this.canvasContext.clip();
-
-    this.canvasContext.save();
-    //this.canvasContext.globalAlpha = this.goalPercentage > 0.4 ? this.goalPercentage : 0.4;
-    this.canvasContext.drawImage(this.backgroundImage, 0, 0, this.canvasSize, this.canvasSize);
-    this.canvasContext.restore();
-
-    this.canvasContext.beginPath();
-    this.canvasContext.arc(
-      this.centerPosition.x,
-      this.centerPosition.y,
-      this.canvasSize / 2,
-      2 * Math.PI,
-      0,
-      true,
-    );
-    this.canvasContext.clip();
-    this.canvasContext.closePath();
-
-    const imageData = this.canvasContext.getImageData(0, 0, this.canvasSize, this.canvasSize);
-
-    if (!this.savedPixels) this.savedPixels = new Uint8ClampedArray(imageData.data);
-
-    for (let i = 0; i < imageData.data.length; i += 4) {
-      const average = (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
-
-      imageData.data[i] = average;
-      imageData.data[i + 1] = average;
-      imageData.data[i + 2] = average;
-      imageData.data[i + 3] = imageData.data[i + 3] ? 100 : imageData.data[i + 3];
-    }
-
-    const start = imageData.width * 4 * Math.round(imageData.height * (1 - this.goalPercentage));
-
-    for (let i = start; i < imageData.data.length; i += 4) {
-      imageData.data[i] = this.savedPixels[i];
-      imageData.data[i + 1] = this.savedPixels[i + 1];
-      imageData.data[i + 2] = this.savedPixels[i + 2];
-      imageData.data[i + 3] = this.savedPixels[i + 3];
-    }
-
-    this.canvasContext.putImageData(imageData, 0, 0);
-
-    this.canvasContext.restore();
-
-    this.canvasContext.beginPath();
-    this.canvasContext.arc(
-      this.centerPosition.x,
-      this.centerPosition.y,
-      this.canvasSize / 2 - Goal.LINE_WIDTH,
-      0,
-      2 * Math.PI,
-      false,
-    );
-    this.canvasContext.arc(
-      this.centerPosition.x,
-      this.centerPosition.y,
-      this.canvasSize / 2,
-      2 * Math.PI,
-      0,
-      true,
-    );
-    this.canvasContext.fillStyle = Goal.INNER_LINE_COLOR;
-    this.canvasContext.fill();
-    this.canvasContext.closePath();
-
-    this.drawProgress();
-  }
-
-  private drawProgress() {
-    if (!this.canvasContext || !this.centerPosition) return;
-
+    const canvasSize = canvasSizeRef.current;
     const th1 = -90 * (Math.PI / 180);
-    const th2 = (this.goalPercentage * 360 - 90) * (Math.PI / 180);
-    const d1 = Goal.applyAngle(this.centerPosition, th1, this.canvasSize / 2);
-    const d2 = Goal.applyAngle(this.centerPosition, th2, this.canvasSize / 2 - Goal.LINE_WIDTH);
-    const magicGradient = this.canvasContext.createLinearGradient(d1.x, d1.y, d2.x, d2.y);
+    const th2 = (goalPercentageRef.current * 360 - 90) * (Math.PI / 180);
+    const d1 = applyAngle(centerPosition, th1, canvasSize / 2);
+    const d2 = applyAngle(centerPosition, th2, canvasSize / 2 - LINE_WIDTH);
+    const magicGradient = canvasContext.createLinearGradient(
+      d1.x,
+      d1.y,
+      d2.x,
+      d2.y,
+    );
 
     magicGradient.addColorStop(0, "#ffc5e6");
     magicGradient.addColorStop(0.25, "#FCBCD7");
@@ -241,43 +56,234 @@ export default class Goal extends React.Component<IGoalProps> {
     magicGradient.addColorStop(0.75, "#EF87BE");
     magicGradient.addColorStop(1, "#E56AB3");
 
-    this.canvasContext.beginPath();
-    this.canvasContext.arc(
-      this.centerPosition.x,
-      this.centerPosition.y,
-      this.canvasSize / 2,
+    canvasContext.beginPath();
+    canvasContext.arc(
+      centerPosition.x,
+      centerPosition.y,
+      canvasSize / 2,
       th1,
       th2,
       false,
     );
-    this.canvasContext.arc(
-      this.centerPosition.x,
-      this.centerPosition.y,
-      this.canvasSize / 2 - Goal.LINE_WIDTH,
+    canvasContext.arc(
+      centerPosition.x,
+      centerPosition.y,
+      canvasSize / 2 - LINE_WIDTH,
       th2,
       th1,
       true,
     );
-    this.canvasContext.fillStyle = magicGradient;
-    this.canvasContext.fill();
-    this.canvasContext.closePath();
-  }
+    canvasContext.fillStyle = magicGradient;
+    canvasContext.fill();
+    canvasContext.closePath();
+  }, []);
 
-  private onResize = () => {
-    if (this.resizeTimeout) clearTimeout(this.resizeTimeout);
+  const draw = useCallback(() => {
+    const canvasContext = canvasContextRef.current;
+    const centerPosition = centerPositionRef.current;
+    const backgroundImage = backgroundImageRef.current;
 
-    this.resizeTimeout = setTimeout(() => {
-      this.resizeTimeout = undefined;
-      this.setup();
-    }, 100);
-  };
+    if (
+      !isMountedRef.current ||
+      !canvasContext ||
+      !centerPosition ||
+      !backgroundLoadedRef.current ||
+      !backgroundImage
+    )
+      return;
 
-  private static applyAngle(point: { x: number; y: number }, angle: number, distance: number) {
-    return {
-      x: point.x + Math.cos(angle) * distance,
-      y: point.y + Math.sin(angle) * distance,
+    const canvasSize = canvasSizeRef.current;
+
+    canvasContext.clearRect(0, 0, canvasSize, canvasSize);
+
+    canvasContext.save();
+
+    canvasContext.beginPath();
+    canvasContext.arc(
+      centerPosition.x,
+      centerPosition.y,
+      canvasSize / 2 - LINE_WIDTH,
+      0,
+      2 * Math.PI,
+      false,
+    );
+    canvasContext.closePath();
+    canvasContext.clip();
+
+    canvasContext.save();
+    //canvasContext.globalAlpha = goalPercentageRef.current > 0.4 ? goalPercentageRef.current : 0.4;
+    canvasContext.drawImage(backgroundImage, 0, 0, canvasSize, canvasSize);
+    canvasContext.restore();
+
+    canvasContext.beginPath();
+    canvasContext.arc(
+      centerPosition.x,
+      centerPosition.y,
+      canvasSize / 2,
+      2 * Math.PI,
+      0,
+      true,
+    );
+    canvasContext.clip();
+    canvasContext.closePath();
+
+    const imageData = canvasContext.getImageData(0, 0, canvasSize, canvasSize);
+
+    if (!savedPixelsRef.current)
+      savedPixelsRef.current = new Uint8ClampedArray(imageData.data);
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      const average =
+        (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) /
+        3;
+
+      imageData.data[i] = average;
+      imageData.data[i + 1] = average;
+      imageData.data[i + 2] = average;
+      imageData.data[i + 3] = imageData.data[i + 3]
+        ? 100
+        : imageData.data[i + 3];
+    }
+
+    const start =
+      imageData.width *
+      4 *
+      Math.round(imageData.height * (1 - goalPercentageRef.current));
+
+    for (let i = start; i < imageData.data.length; i += 4) {
+      imageData.data[i] = savedPixelsRef.current[i];
+      imageData.data[i + 1] = savedPixelsRef.current[i + 1];
+      imageData.data[i + 2] = savedPixelsRef.current[i + 2];
+      imageData.data[i + 3] = savedPixelsRef.current[i + 3];
+    }
+
+    canvasContext.putImageData(imageData, 0, 0);
+
+    canvasContext.restore();
+
+    canvasContext.beginPath();
+    canvasContext.arc(
+      centerPosition.x,
+      centerPosition.y,
+      canvasSize / 2 - LINE_WIDTH,
+      0,
+      2 * Math.PI,
+      false,
+    );
+    canvasContext.arc(
+      centerPosition.x,
+      centerPosition.y,
+      canvasSize / 2,
+      2 * Math.PI,
+      0,
+      true,
+    );
+    canvasContext.fillStyle = INNER_LINE_COLOR;
+    canvasContext.fill();
+    canvasContext.closePath();
+
+    drawProgress();
+  }, [drawProgress]);
+
+  const setup = useCallback(() => {
+    if (!isMountedRef.current) return;
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const toOperate = windowWidth < windowHeight ? windowWidth : windowHeight;
+
+    if (toOperate <= CIRCLE_MAX_SIZE) {
+      canvasSizeRef.current = toOperate;
+    } else {
+      if (canvasSizeRef.current !== CIRCLE_MAX_SIZE)
+        canvasSizeRef.current = CIRCLE_MAX_SIZE;
+    }
+
+    if (!canvasRef.current) return;
+
+    savedPixelsRef.current = undefined;
+    canvasRef.current.width = canvasSizeRef.current;
+    canvasRef.current.height = canvasSizeRef.current;
+
+    centerPositionRef.current = {
+      x: canvasSizeRef.current / 2,
+      y: canvasSizeRef.current / 2,
     };
-  }
+
+    if (!canvasContextRef.current) {
+      canvasContextRef.current = canvasRef.current.getContext("2d", {
+        willReadFrequently: true,
+      });
+    }
+
+    const backgroundImage = backgroundImageRef.current;
+    if (!backgroundImage) return;
+
+    if (backgroundLoadedRef.current) draw();
+    else {
+      backgroundImage.src = resolveGoalImageUrl("kaaajk4-love");
+      backgroundImage.onload = () => {
+        if (!isMountedRef.current) return;
+
+        backgroundLoadedRef.current = true;
+        draw();
+      };
+    }
+  }, [draw]);
+
+  const onResize = useCallback(() => {
+    if (resizeTimeoutRef.current) clearTimeout(resizeTimeoutRef.current);
+
+    resizeTimeoutRef.current = setTimeout(() => {
+      resizeTimeoutRef.current = undefined;
+      setup();
+    }, 100);
+  }, [setup]);
+
+  useEffect(() => {
+    isMountedRef.current = true;
+    setup();
+
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      isMountedRef.current = false;
+
+      if (resizeTimeoutRef.current) {
+        clearTimeout(resizeTimeoutRef.current);
+        resizeTimeoutRef.current = undefined;
+      }
+
+      savedPixelsRef.current = undefined;
+
+      if (backgroundImageRef.current) {
+        backgroundImageRef.current.onload = null;
+      }
+
+      window.removeEventListener("resize", onResize);
+    };
+  }, [setup, onResize]);
+
+  useEffect(() => {
+    goalPercentageRef.current = current / goal;
+
+    if (goalPercentageRef.current > 1) goalPercentageRef.current = 1;
+    else draw();
+  }, [current, goal, draw]);
+
+  return (
+    <div className={"goal"}>
+      {type === "subs" && <h5>Cel subów</h5>}
+      {type === "followers" && <h5>Cel followów</h5>}
+
+      <canvas ref={canvasRef} />
+
+      <div className={"text"}>
+        {current} / {goal}
+      </div>
+    </div>
+  );
 }
 
 interface IGoalProps {
