3fd759b3a20534309b61e9d1a9b4fdbd4cbec4d3
Michał Szwindowski <github@nedi.me>
2026-05-26 07:45:33 +0200

    chore(config): remove unused decorators Babel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 5173,
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
 });
