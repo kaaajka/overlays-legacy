72336134bafc5e32b0ad0b01e100cda2a4e4d24d
Michał Szwindowski <github@nedi.me>
2026-05-26 07:49:12 +0200

    refactor(models): remove redundant normal event constructor

--- src/models/NormalEvent.tsx
+++ src/models/NormalEvent.tsx
@@ -1,14 +1,10 @@
-import { EventModel, type IEventModelSchema } from "./Event";
+import { EventModel } from './Event';
 
 export class NormalEventModel extends EventModel {
   time: number | undefined = undefined;
 
-  constructor(data: IEventModelSchema) {
-    super(data);
-  }
-
   get formattedTime(): string | null {
-    if (typeof this.time === "undefined") return null;
+    if (typeof this.time === 'undefined') return null;
 
     const hours = Math.floor(this.time / 3600);
     const minutes = Math.floor(this.time / 60) % 60;
@@ -16,7 +12,7 @@ export class NormalEventModel extends EventModel {
 
     return [hours, minutes, seconds]
       .map((v) => (v < 10 ? `0${v}` : v))
-      .filter((v, i) => v !== "00" || i > 0)
-      .join(":");
+      .filter((v, i) => v !== '00' || i > 0)
+      .join(':');
   }
 }
